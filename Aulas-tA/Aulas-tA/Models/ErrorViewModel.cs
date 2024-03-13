309c0b9091dce581fac771ab8e41e4e236f2ad59
Joao Correia <aluno21302@ipt.pt>
2024-03-13 10:46:55 +0000

    Criação do projeto

---
+++ Aulas-tA/Aulas-tA/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Aulas_tA.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
