60617b8824542dc1f12271110412f1f37c339faf
Joao Correia <aluno21302@ipt.pt>
2024-03-13 11:01:09 +0000

    Início da criação do Model "Cursos"

---
+++ Aulas-tA/Aulas-tA/Models/Cursos.cs
@@ -0,0 +1,10 @@
+﻿namespace Aulas_tA.Models
+{
+    public class Cursos
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Logotipo { get; set; }     
+
+    }
+}
